7515b71922829836913c60b03841084b06900fb7
Rob <robwagner33@gmail.com>
2017-10-29 16:13:25 -0400

    Unlinked games from tournaments

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -2,36 +2,35 @@ require 'test_helper'
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @tournament = tournaments(:one)
     @game = games(:one)
   end
 
   test "should get index" do
-    get tournament_games_url(@tournament), as: :json
+    get games_url, as: :json
     assert_response :success
   end
 
   test "should create game" do
     assert_difference('Game.count') do
-      post tournament_games_url(@tournament), params: { tournament_id: @tournament.id, game: { name: @game.name, scoring: @game.scoring } }, as: :json
+      post games_url, params: { game: { name: @game.name, scoring: @game.scoring } }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show game" do
-    get tournament_game_url(@tournament, @game), as: :json
+    get game_url(@game), as: :json
     assert_response :success
   end
 
   test "should update game" do
-    patch tournament_game_url(@tournament, @game), params: { tournament_id: @tournament.id, game: { name: @game.name, scoring: @game.scoring } }, as: :json
+    patch game_url(@game), params: { game: { name: @game.name, scoring: @game.scoring } }, as: :json
     assert_response 200
   end
 
   test "should destroy game" do
     assert_difference('Game.count', -1) do
-      delete tournament_game_url(@tournament, @game), as: :json
+      delete game_url(@game), as: :json
     end
 
     assert_response 204
