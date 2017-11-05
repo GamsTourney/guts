c5051bd581f1cb914aae806791744483815af307
Rob <robwagner33@gmail.com>
2017-11-05 14:21:21 -0500

    [fix] bad refactor]

--- db/migrate/20171105165012_rename_participations_to_competitors.rb
+++ db/migrate/20171105165012_rename_participations_to_competitors.rb
@@ -1,5 +1,5 @@
 class RenameParticipationsToCompetitors < ActiveRecord::Migration[5.1]
   def change
-    rename_table :competitors, :competitors
+    rename_table :participations, :competitors
   end
 end
