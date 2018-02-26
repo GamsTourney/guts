8e3b8778b3c5b2c57d3915c041701c4763c31a4b
robwagner33@gmail.com <robwagner33@gmail.com>
2018-02-26 16:59:14 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :tournaments, defaults: {format: :json} do
-    member do
-      get :standings
-    end
     resources :matches, defaults: {format: :json}, only: [:index, :create]
   end
   resources :matches, defaults: {format: :json}, only: [:show, :destroy, :update] do
