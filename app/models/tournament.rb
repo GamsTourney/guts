1dbc2b02880da4197b4d434b2a19c41b0c4fe6d3
robwagner33@gmail.com <robwagner33@gmail.com>
2019-02-16 12:12:03 -0500

    added icy

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -46,4 +46,3 @@ class Tournament < ApplicationRecord
     }
   end
 end
-
--- db/seeds/2019.rb
+++ db/seeds/2019.rb
@@ -11,3 +11,8 @@ tournament.players = players
 # Games
 games = YAML::load_file("db/seeds/2019/games.yml")
 Game.create(games)
+
+# All Day
+icy = Game.find_by(name: 'Icy Tower')
+Helpers.add_scores(icy, [8, 6, 4, 3, 2, 1, 0, 0])
+Helpers.create_matches(tournament, [[[1,2,3,4,5,6,7,8]]], tournament_start, 9.hours, [icy], true)
