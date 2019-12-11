73d6dd6c591dcdbb0db3d5448025b428d99be2d8
Rob Wagner <robwagner33@gmail.com>
2019-12-10 21:06:43 -0500

    Update base of guts for 2020

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,10 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'glory-dev.herokuapp.com', 'www.gambos.biz'
-
+    if Rails.env.development?
+      origins 'http://localhost:3001'
+    elsif Rails.env.production?
+      origins 'glory-dev.herokuapp.com', 'www.gambos.biz'
+    end
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
